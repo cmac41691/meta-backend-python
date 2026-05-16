35eb88d3e2c9d5576b08017db04f4fe2056977e0
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2026-05-16 07:51:34 -0400

    feat: add import statement and alias examples

---
+++ exercises/modules-packages/helper_module.py
@@ -0,0 +1,22 @@
+# Import Statements Examples
+
+# -------------------------
+# Standard module import
+# -------------------------
+
+import math
+
+root = math.sqrt(100)
+
+print(root)
+
+
+# -------------------------
+# Alias import example
+# -------------------------
+
+from math import sqrt as m
+
+root = m(25)
+
+print(root)

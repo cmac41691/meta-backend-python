b38b53c5502dd7d37dcbd88bee6528f5d0091f58
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2026-05-28 16:54:26 -0400

    chore: setup pytest string validation exercise

---
+++ exercises/testing-tools/spellcheck.py
@@ -0,0 +1,36 @@
+def input_value():
+    # Default input value for testing
+    input = alpha
+    return input
+
+# First test function test_length()
+def test_length(input_value):
+    """ 
+    Tests whether a string has fewer than 10 words and fewer than 50 chars.
+
+    [IMPLEMENT ME]
+        1. Use an assert statement to check the given string has fewer than 10 words
+        2. Use an assert statement to check the given string has fewer than 50 chars
+
+    Args:
+      input_value: a function that returns a string, which can be configured
+                   in the input_value() function
+    """
+    ### WRITE SOLUTION CODE HERE
+
+# Second test function test_struc()
+def test_struc(input_value):
+    """ 
+    Tests whether a string begins with a capital letter and ends with a period.
+
+    [IMPLEMENT ME]
+        1. Use an assert statement to check the given string begins with a capital letter
+        2. Use an assert statement to check the given string ends with a period ('.')
+
+    Args:
+      input_value: a function that returns a string, which can be configured
+                   in the input_value() function
+    """
+    ### WRITE SOLUTION CODE HERE
+
+# Run these tests with `python3 -m pytest test_spellcheck.py`

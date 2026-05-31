60b586dc9bbe20c3a366f88d87f7ea8e616da517
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2026-05-31 10:00:07 -0400

    Complete Pytest string validation exercise

--- exercises/testing-tools/spellcheck.py
+++ exercises/testing-tools/spellcheck.py
@@ -1,36 +1,23 @@
-def input_value():
-    # Default input value for testing
-    input = alpha
-    return input
+def word_count(sentence):
+    # Function to check the number of words. Returns the word count in string.
+    words = len(sentence.split())
+    print(words)
+    return words
 
-# First test function test_length()
-def test_length(input_value):
-    """ 
-    Tests whether a string has fewer than 10 words and fewer than 50 chars.
+def char_count(sentence):
+    # Function to check the number of characters. Returns the character count in string.
+    chars = len(sentence)
+    print(chars)
+    return chars
 
-    [IMPLEMENT ME]
-        1. Use an assert statement to check the given string has fewer than 10 words
-        2. Use an assert statement to check the given string has fewer than 50 chars
+def first_char(sentence):
+    # Function to check the first character using the string index. Returns the first character in string.
+    first = sentence[0]
+    return first
 
-    Args:
-      input_value: a function that returns a string, which can be configured
-                   in the input_value() function
-    """
-    ### WRITE SOLUTION CODE HERE
+def last_char(sentence):
+    # Function to check the last character using the string index. Returns the last character in string.
+    last = sentence[-1]
+    return last
 
-# Second test function test_struc()
-def test_struc(input_value):
-    """ 
-    Tests whether a string begins with a capital letter and ends with a period.
 
-    [IMPLEMENT ME]
-        1. Use an assert statement to check the given string begins with a capital letter
-        2. Use an assert statement to check the given string ends with a period ('.')
-
-    Args:
-      input_value: a function that returns a string, which can be configured
-                   in the input_value() function
-    """
-    ### WRITE SOLUTION CODE HERE
-
-# Run these tests with `python3 -m pytest test_spellcheck.py`
--- exercises/testing-tools/test_spellcheck.py
+++ exercises/testing-tools/test_spellcheck.py
@@ -1,21 +1,67 @@
-def word_count(sentence):
-    # Function to check the number of words. Returns the word count in string.
-    words = len(sentence.split())
-    print(words)
-    return words
+import pytest
+from spellcheck import word_count, char_count, first_char, last_char
 
-def char_count(sentence):
-    # Function to check the number of characters. Returns the character count in string.
-    chars = len(sentence)
-    print(chars)
-    return chars
+alpha = "Checking the lenght & structure of the sentence."
+beta = "This sentence should fail the test"
 
-def first_char(sentence):
-    # Function to check the first character using the string index. Returns the first character in string.
-    first = sentence[0]
-    return first
+@pytest.fixture
+def input_value():
+    # Default input value for testing
+    input = alpha
+    return input
 
-def last_char(sentence):
-    # Function to check the last character using the string index. Returns the last character in string.
-    last = sentence[-1]
-    return last
+# First test function test_length()
+def test_length(input_value):
+
+    assert word_count(input_value) < 10
+    assert char_count(input_value) < 50
+
+
+    print(input_value)
+    print(type(input_value))
+    """ 
+    Tests whether a string has fewer than 10 words and fewer than 50 chars.
+
+    [IMPLEMENT ME]
+        1. Use an assert statement to check the given string has fewer than 10 words
+        2. Use an assert statement to check the given string has fewer than 50 chars
+
+    Args:
+      input_value: a function that returns a string, which can be configured
+                   in the input_value() function
+    """
+    ### WRITE SOLUTION CODE HERE
+
+
+# Second test function test_struc()
+def test_struc(input_value):
+
+    assert first_char(input_value).isupper()
+    assert last_char(input_value) == "."    
+    """ 
+    Tests whether a string begins with a capital letter and ends with a period.
+
+    [IMPLEMENT ME]
+        1. Use an assert statement to check the given string begins with a capital letter
+        2. Use an assert statement to check the given string ends with a period ('.')
+
+    Args:
+      input_value: a function that returns a string, which can be configured
+                   in the input_value() function
+    """
+    ### WRITE SOLUTION CODE HERE
+
+# Run these tests with `python3 -m pytest test_spellcheck.py`
+
+
+
+
+
+
+
+
+
+
+
+
+
